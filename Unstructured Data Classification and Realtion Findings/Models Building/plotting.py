53e006fd3e19118884109067e044c0908dfd6979
Social-Cognitive <108185893+Social-Cognitive@users.noreply.github.com>
2022-06-29 23:34:22 +0600

    Add files via upload

---
+++ Unstructured Data Classification and Realtion Findings/Models Building/plotting.py
@@ -0,0 +1,152 @@
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import confusion_matrix
+import scikitplot as skplt
+import matplotlib.pyplot as plt
+from keras.models import load_model
+
+
+
+
+def plot_loss_and_acc(history):
+    train_loss = history.history["loss"]
+    train_acc = history.history["accuracy"]
+    val_loss = history.history["val_loss"]
+    val_acc = history.history["val_accuracy"]
+
+    fig = plt.figure(figsize=(12,5))
+
+    ax = fig.add_subplot(121)
+    ax.plot(range(len(train_loss)), train_loss, label="Train Loss");
+    ax.plot(range(len(val_loss)), val_loss, label="Validation Loss");
+    plt.xlabel("Epochs"); plt.ylabel("Loss");
+    plt.title("Train Loss vs Validation Loss");
+    plt.legend(loc="best");
+
+    ax = fig.add_subplot(122)
+    ax.plot(range(len(train_acc)), train_acc, label="Train Accuracy");
+    ax.plot(range(len(val_acc)), val_acc, label="Validation Accuracy");
+    plt.xlabel("Epochs"); plt.ylabel("Accuracy");
+    plt.title("Train Accuracy vs Validation Accuracy");
+    plt.legend(loc="best");
+
+
+
+target_classes = [0,1]
+def performance(model, X_train_vect,X_test_vect,Y_train,Y_test,model_name):
+  model = load_model(model_name)
+  train_preds = model.predict(X_train_vect)
+  test_preds = model.predict(X_test_vect)
+  tracc = model.evaluate(X_train_vect,Y_train)[1]
+  acc= model.evaluate(X_test_vect,Y_test)[1]
+  print("Train Accuracy : {}".format(tracc))
+  print("Test  Accuracy : {}".format(acc))
+  y_pred = (test_preds > 0.5)
+  print("\nClassification Report : ")
+  print(classification_report(Y_test, y_pred))
+  y_pred = y_pred.astype(int)
+  y_pred = y_pred.reshape(y_pred.shape[0])
+  
+  skplt.metrics.plot_confusion_matrix([target_classes[i] for i in Y_test], [target_classes[i] for i in y_pred],
+                                    normalize=True,
+                                    title="Confusion Matrix",
+                                    cmap="Blues",
+                                    hide_zeros=True,
+                                    figsize=(5,5)
+                                    );
+
+
+def word_cloud(data,classno):
+
+  all_words = ' '.join([text for text in data])
+
+
+  from wordcloud import WordCloud
+  wordcloud = WordCloud(width=800, height=500, random_state=21, max_font_size=110).generate(all_words)
+
+  plt.figure(figsize=(10, 7))
+  plt.imshow(wordcloud, interpolation="bilinear")
+  plt.axis('off')
+  plt.title("Most Common words in group "+str(classno))
+  plt.show()
+
+
+
+
+# uncomment if gensim is installed
+#!pip install gensim
+import gensim
+# Need the interactive Tools for Matplotlib
+#%matplotlib notebook
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import seaborn as sns
+import pandas as pd
+# tsne plot for below word
+# for_word = 'food'
+def tsne_plot(for_word, w2v_model):
+    # trained word2vec model dimention
+    dim_size = w2v_model.wv.vectors.shape[1]
+
+    arrays = np.empty((0, dim_size), dtype='f')
+    word_labels = [for_word]
+    color_list  = ['red']
+
+    # adds the vector of the query word
+    arrays = np.append(arrays, w2v_model.wv.__getitem__([for_word]), axis=0)
+
+    # gets list of most similar words
+    sim_words = w2v_model.wv.most_similar(for_word, topn=6)
+
+    # adds the vector for each of the closest words to the array
+    for wrd_score in sim_words:
+        wrd_vector = w2v_model.wv.__getitem__([wrd_score[0]])
+        word_labels.append(wrd_score[0])
+        color_list.append('green')
+        arrays = np.append(arrays, wrd_vector, axis=0)
+
+    #---------------------- Apply PCA and tsne to reduce dimention --------------
+
+    # fit 2d PCA model to the similar word vectors
+    model_pca = PCA(n_components = 6).fit_transform(arrays)
+
+    # Finds 2d coordinates t-SNE
+    np.set_printoptions(suppress=True)
+    Y = TSNE(n_components=2, random_state=0, perplexity=15).fit_transform(model_pca)
+
+    # Sets everything up to plot
+    df_plot = pd.DataFrame({'x': [x for x in Y[:, 0]],
+                       'y': [y for y in Y[:, 1]],
+                       'words_name': word_labels,
+                       'words_color': color_list})
+
+    #------------------------- tsne plot Python -----------------------------------
+
+    # plot dots with color and position
+    plot_dot = sns.regplot(data=df_plot,
+                     x="x",
+                     y="y",
+                     fit_reg=False,
+                     marker="o",
+                     scatter_kws={'s': 40,
+                                  'facecolors': df_plot['words_color']
+                                 }
+                    )
+
+    # Adds annotations with color one by one with a loop
+    for line in range(0, df_plot.shape[0]):
+         plot_dot.text(df_plot["x"][line],
+                 df_plot['y'][line],
+                 '  ' + df_plot["words_name"][line].title(),
+                 horizontalalignment='left',
+                 verticalalignment='bottom', size='medium',
+                 color=df_plot['words_color'][line],
+                 weight='normal'
+                ).set_size(10)
+
+
+    plt.xlim(Y[:, 0].min()-50, Y[:, 0].max()+50)
+    plt.ylim(Y[:, 1].min()-50, Y[:, 1].max()+50)
+
+    plt.title('t-SNE visualization for word "{}'.format(for_word.title()) +'"')
